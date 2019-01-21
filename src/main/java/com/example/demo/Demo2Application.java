65668e33f8cedd072a31988bb050eaab070a6973
Sambath Mom <sambathmom06@gmail.com>
2019-01-21 08:09:05 +0700

    test

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 // @SpringBootConfiguration
-// @ComponentScan
+@ComponentScan
 public class Demo2Application {
 
 	public static void main(String[] args) {
