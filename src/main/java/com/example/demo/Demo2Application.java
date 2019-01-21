b5eed63c047042f617066bdc50f0bc3074efd6ef
Sambath Mom <sambathmom06@gmail.com>
2019-01-21 08:02:28 +0700

    Comment in main

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@SpringBootConfiguration
-@ComponentScan
+// @SpringBootConfiguration
+// @ComponentScan
 public class Demo2Application {
 
 	public static void main(String[] args) {
