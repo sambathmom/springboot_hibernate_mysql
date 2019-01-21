420351e4664de4660f55c89fe6b972460f0c08f2
Sambath Mom <sambathmom06@gmail.com>
2019-01-21 08:12:59 +0700

    K

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-// @SpringBootConfiguration
+@SpringBootConfiguration
 @ComponentScan
 public class Demo2Application {
 
